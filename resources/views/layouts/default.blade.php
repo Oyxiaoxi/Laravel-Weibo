d6af4a8c893f9f47950284203c192fde1abca367
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2020-12-17 05:46:36 +0000

    layouts

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title>@yield('title', 'Weibo App') - Laravel 新手入门</title>
+    </head>
+
+    <body>
+        @yield('content')
+    </body>
+
+</html>
