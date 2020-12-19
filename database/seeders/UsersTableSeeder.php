02419a55e49de81983b148d1cfbb252a5218372d
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2020-12-19 03:55:00 +0000

    Destroy

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Oyxiaoxi';
         $user->email = 'Oyxiaoxi@example.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
